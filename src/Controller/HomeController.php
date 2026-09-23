2f611698ec0c8dc795aa33be2385da03099f7e47
kkamara <kamaracomputers@gmail.com>
2026-09-23 13:55:05 +0100

    Add HomeController::class

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    #[Route('/')]
+    public function index()
+    {
+        return new Response("Hello, World!");
+    }
+}
