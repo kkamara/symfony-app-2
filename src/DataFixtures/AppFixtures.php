c13dcfcfdfd713e72837a4c5a50cad465093a426
kkamara <kamaracomputers@gmail.com>
2026-09-24 14:44:51 +0100

    Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,26 @@
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $product = new Product();
+        $product->setName('Product One');
+        $product->setDescription("This is the first description.");
+        $product->setSize(100);
+
+        $manager->persist($product);
+
+        $product = new Product();
+        $product->setName('Product Two');
+        $product->setDescription("This is the second description.");
+        $product->setSize(200);
+
+        $manager->persist($product);
 
         $manager->flush();
     }
