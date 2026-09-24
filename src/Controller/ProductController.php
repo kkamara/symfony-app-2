6d486ae40dd6e222083da29766ecf0c31f45e1c0
kkamara <kamaracomputers@gmail.com>
2026-09-24 14:52:35 +0100

    Getting & displaying product data

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Product;
 
 final class ProductController extends AbstractController
 {
     #[Route('/products', name: 'product_index')]
-    public function index(): Response
+    public function index(ProductRepository $repo): Response
     {
         return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $repo->findAll(),
         ]);
     }
 }
